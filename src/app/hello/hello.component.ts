8b53aa22a5480d19b0c1a54c90b7243e0bd3945e
grace7937 <eunchong.k153@gmail.com>
2021-11-22 23:21:29 +0900

    Start angular practice

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector : 'app-hello',
+    templateUrl : './hello.component.html',
+    styleUrls : ['./hello.component.css']
+})
+export class HellowComponent {
+
+}
