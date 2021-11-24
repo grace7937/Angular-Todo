52a1d6114a0c95da993ffd9bdd878117737716a1
grace7937 <eunchong.k153@gmail.com>
2021-11-24 22:37:11 +0900

    Add app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HellowComponent } from './hello/hello.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HellowComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
